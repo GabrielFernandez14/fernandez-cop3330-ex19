7240532ed509e7c3f39cfa38662024cf05878d1b
Gabriel Desktop <gabrielfernandez@knights.ucf.edu>
2021-05-29 22:41:29 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,67 @@
+package org.example;
+import java.util.Scanner;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
+public class App
+{
+
+    // Main function
+    public static void main( String[] args )
+    {
+        // Declare constants and initialize string prompts
+        final double IDEAL_LOWER = 18.5;
+        final double IDEAL_HIGHER = 25;
+        String weightPrompt = "What is your weight? (lbs)";
+        String heightPrompt = "What is your height? (inches)";
+
+        // Loop user input until they enter a number, then store
+        // that value
+        double weight = getDouble(weightPrompt);
+        double height = getDouble(heightPrompt);
+
+        // Round to two decimal places
+        DecimalFormat df = new DecimalFormat("0.00");
+        df.setRoundingMode(RoundingMode.CEILING);
+
+        // Calculate the BMI
+        double bmi = (weight / (height * height)) * 703;
+
+        // Print the result
+        System.out.println("Your BMI is " + df.format(bmi) + ".");
+
+        // If the user's BMI is under the lower bound, the user is underweight
+        if (bmi < IDEAL_LOWER) {
+            System.out.println("You are underweight. You should see your doctor.");
+        }
+        // If the user's BMI is between both bounds, the user is in the ideal range
+        else if (bmi >= IDEAL_LOWER && bmi <= IDEAL_HIGHER) {
+            System.out.println("You are within the ideal weight range.");
+        }
+        // If the user's BMI is neither under or between the ideal range, that means
+        // they are above the higher bound, so they are overweight
+        else {
+            System.out.println("You are overweight. You should see your doctor.");
+        }
+    }
+
+    // Yes this function is copy pasted from exercise 17, sue me
+    // The function loops until the user enters a number
+    public static double getDouble(String prompt) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(prompt);
+
+        // Unfortunately, if the user decides to input multiple strings
+        // the while loop will output this message for the amount of strings
+        // inputted, but still works in the sense that it does not allow the user
+        // to continue until an integer is inputted
+        while (!sc.hasNextDouble()) {
+            System.out.println("Sorry, you must enter a number, please try again.");
+            sc.next();
+        }
+
+        // If we have exited the while loop, that means we had an integer
+        // be inputted, so we can return that int
+        return sc.nextDouble();
+    }
+}
